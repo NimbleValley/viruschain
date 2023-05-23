3cd0cd7cda5b3f5f809b96a16eddc57da8663d96
Mason McManus <97319135+NimbleValley@users.noreply.github.com>
2023-05-23 08:19:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const windowFeatures = "left=100,top=100,width=320,height=520";
+const background = document.getElementById("background");
 
-window.open(document.URL, '_blank', `location=yes,left=${Math.random() * 500},top=${Math.random() * 500},height=570,width=520,scrollbars=yes,status=yes`);
+var images = ["inspo.jpg", "CAPTAINE_MATTHEW.jpg", "faculty2088_small_2.jfif"]
+
+background.style.backgroundImage = `url(${images[Math.floor(Math.random() * images.length)]})`;
+
+for (var i = 0; i < 2; i++) {
+    window.open(document.URL, '_blank', `location=yes,left=${Math.random() * 500},top=${Math.random() * 500},height=570,width=520,scrollbars=yes,status=yes`);
+}
