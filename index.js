1dcf3e09cfc81f68c33428952acdeaa740247877
Mason McManus <97319135+NimbleValley@users.noreply.github.com>
2023-05-23 08:00:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+const windowFeatures = "left=100,top=100,width=320,height=520";
+
+window.open(document.URL, '_blank', `location=yes,left=${Math.random() * 500},top=${Math.random() * 500},height=570,width=520,scrollbars=yes,status=yes`);
