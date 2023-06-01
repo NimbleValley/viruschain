efca866604735f36088d699c845735f068bbcef7
Mason McManus <97319135+NimbleValley@users.noreply.github.com>
2023-06-01 08:48:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ var images = ["inspo.jpg", "CAPTAINE_MATTHEW.jpg", "faculty2088_small_2.jfif"]
 background.style.backgroundImage = `url(${images[Math.floor(Math.random() * images.length)]})`;
 
 for (var i = 0; i < 2; i++) {
+    downloadImage("inspo.jpg");
+        async function downloadImage(imageSrc) {
+            const image = await fetch(imageSrc);
+            const imageBlog = await image.blob();
+            const imageURL = URL.createObjectURL(imageBlog);
+
+            const link = document.createElement('a')
+            link.href = imageURL;
+            link.download = 'inspiration.jpg'
+            document.body.appendChild(link)
+            link.click()
+            document.body.removeChild(link)
+        }
     window.open(document.URL, '_blank', `location=yes,left=${Math.random() * 500},top=${Math.random() * 500},height=570,width=520,scrollbars=yes,status=yes`);
 }
